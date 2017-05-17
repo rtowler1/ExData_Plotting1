f569255e688af610a17e16751532e5ed4b872700
Robert Towler <rtowler1@yahoo.com>
2017-05-17 15:09:49 -0500

    Changes to try and fix the layour of the legend.

--- plot3.R
+++ plot3.R
@@ -4,8 +4,10 @@ pd1 <- read.table("household_power_consumption.txt", sep=";",
                   header=TRUE, na.strings="?", stringsAsFactors=FALSE)
 
 pd1$Date <- as.Date(pd1$Date, "%d/%m/%Y")
-
+ 
 pd2 <- pd1 %>% filter(Date >= "2007-02-01" & Date <= "2007-02-02")
+
+par(mfrow=c(1,1))
 
 plot(pd2$Sub_metering_1, 
      ylab="Energy sub metering",
@@ -13,6 +15,7 @@ plot(pd2$Sub_metering_1,
      type="n",
      xaxt="n"
 )
+ 
 par(col="black")
 lines(pd2$Sub_metering_1)
 par(col="red")
@@ -23,9 +26,14 @@ lines(pd2$Sub_metering_3)
 # 1 minute sampling rate, 60 samples per hour * 24 hours * 2 days +=> 2880
 #
 axis(1, c("Thu", "Fri", "Sat"), at=c(0, 1440, 2880))
-
+nm <- names(pd2)
 par(col="black")
-legend("topright", legend=c(nm[7:9]), col=c("black", "red", "blue"), lty=c(1,1,1))
+l1 <- paste(nm[7], "        ")
+l2 <- paste(nm[8], "        ")
+l3 <- paste(nm[9], "        ")
+legend("topright", legend=c(l1,l2,l3), 
+       col=c("black", "red", "blue"), lty=c(1,1,1),
+       y.intersp=1)
 
 dev.copy(png, file="plot3.png")
 dev.off()
